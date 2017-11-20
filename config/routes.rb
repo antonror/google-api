714402100b2a459d81c395e7f2c4d224466ff14d
antonror <hitty@i.ua>
2017-11-20 12:22:37 +0200

    Caledars: view, controller action and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root to: 'application#calendars'
-  devise_for :users, controllers: { omniauth_callbacks: 'callbacks' } do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
 end
